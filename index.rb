14135c05f570a10a68885ce5563fd31b2e255bb0
hendricksonweib <hendricksonrocha22@gmail.com>
2024-05-20 23:44:51 -0300

    first commit

---
+++ index.rb
@@ -0,0 +1,14 @@
+# print "digite um numero:  "
+# A = gets.chomp.to_i
+# print "digite outro numero:  "
+# B = gets.chomp.to_i
+# puts "Seu resultado é #{A+B}"
+
+print "DIGITE SEU NOME:   "
+Nome = gets.chomp 
+print "DIGITE SEU SOBRENOME:  "
+SobreNome = gets.chomp
+print = "DIGITE SUA IDADE: "
+Idade = gets.chomp.to_i
+
+puts "Olá! seu nome é #{Nome + SobreNome}, você tem #{Idade}!"
